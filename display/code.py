6c2b4c58876c14e2d271ccbf5e52951d9d7e8e6d
Ryan Johnston <ryjo1026@gmail.com>
2021-08-25 00:25:05 -0400

    Initial display code working

--- display/code.py
+++ display/code.py
@@ -1,4 +1,7 @@
 import time
+import adafruit_datetime
+
+import json
 
 import adafruit_display_text.label
 import adafruit_display_shapes.rect
@@ -18,8 +21,13 @@ from adafruit_esp32spi import adafruit_esp32spi
 
 from secrets import secrets
 
-import mta
+# My station: 14th and 6
+STOP_ID = 'D19'
 
+# ------------------------------------------------------------------------------------------------------
+# SETUP
+# ------------------------------------------------------------------------------------------------------
+# ESP wifi setup
 esp32_cs = DigitalInOut(board.ESP_CS)
 esp32_ready = DigitalInOut(board.ESP_BUSY)
 esp32_reset = DigitalInOut(board.ESP_RESET)
@@ -27,11 +35,7 @@ spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
 wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets)
 
-resp = wifi.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm', headers={'x-api-key': secrets['mta-api-key']})
-print(resp.content)
-
-f = mta.Feed()
-
+# Display setup
 displayio.release_displays()
 matrix = rgbmatrix.RGBMatrix(
     width=64, bit_depth=4,
@@ -55,80 +59,165 @@ matrix = rgbmatrix.RGBMatrix(
 )
 display = framebufferio.FramebufferDisplay(matrix)
 
-SYMBOL_FONT = bitmap_font.load_font("/siji_mta.bdf")
+
+def get_departure_times():
+    resp = wifi.get(
+        'https://bwpddnvln1.execute-api.us-east-1.amazonaws.com/dev/{}'.format(STOP_ID),
+        headers={"x-api-key": secrets['ryan_personal_api_key']}
+    )
+    d = resp.json()
+    resp.close()
+
+    return d
 
 
-TEXT_COLOR = 0x222222
-TRAIN_COLOR_ORANGE_FM = 0xcc461b
+# How many times to query the API (seconds)
+API_REFRESH_DELAY = 30
+
+
 ARROW_X = -2
-TRAIN_SYMBOL_X = 7
+TRAIN_SYMBOL_X = 9
 RIGHT_ALIGN_X = 64
 
 TOP_HALF_Y = 8
 BOTTOM_HALF_Y = 24
 
-# Top Half
-top_half = displayio.Group()
-# Up Arrow
-top_half.append(adafruit_display_text.label.Label(
-    SYMBOL_FONT,
-    color=TEXT_COLOR,
-    text="\uE12b",
-    x=ARROW_X,
-    y=TOP_HALF_Y - 1
-))
-# Train Symbol
-top_half.append(adafruit_display_text.label.Label(
-    SYMBOL_FONT,
-    color=TRAIN_COLOR_ORANGE_FM,
-    text="F",
-    x=TRAIN_SYMBOL_X,
-    y=TOP_HALF_Y
-))
-# top_half.append(make_train_symbol(TRAIN_SYMBOL_X, TOP_HALF_Y - 1, TRAIN_COLOR_ORANGE_FM))
-# Train Text
-top_half.append(adafruit_display_text.label.Label(
-    terminalio.FONT,
-    color=TEXT_COLOR,
-    text="5 Min",
-    anchor_point=(1.0, 0.5),
-    anchored_position=(RIGHT_ALIGN_X, TOP_HALF_Y - 2)
-))
+
+class TimeBoard:
+    # X-Values (Where each slide-piece is placed horizontally)
+    ARROW_X = -2
+
+    # Style
+    SYMBOL_FONT = bitmap_font.load_font("/siji_mta.bdf")
+    TEXT_COLOR = 0x222222
+    TRAIN_COLOR_ORANGE_FM = 0xcc461b
+
+    UP_ARROW = '\uE12b'
+    DOWN_ARROW = '\uE12c'
+
+    def __init__(self, departure_times):
+        # Arrays to hold "slides" - A slide is a self-contained group showing the departure time including icon, time, and arrow
+        # The first item in the array is always displayed so to rotate the display, we roatate the array
+        self.uptown_slides = []
+        self.downtown_slides = []
+
+        # The parent group for the actual displayio layers that make up the board
+        self.time_board_group = displayio.Group()
+
+        self.update_board(departure_times)
+
+    # Make an "empty slide" for use when there are no trains
+    def make_empty_slide(self, arrow, y_val):
+        # Slide is list, not displayio.Group because displayio.Group has issues
+        slide = []
+
+        slide.append(adafruit_display_text.label.Label(
+            self.SYMBOL_FONT,
+            color=self.TEXT_COLOR,
+            text=arrow,
+            x=ARROW_X,
+            y=y_val
+        ))
+        slide.append(adafruit_display_text.label.Label(
+            terminalio.FONT,
+            color=self.TEXT_COLOR,
+            text="No trains",
+            anchor_point=(1.0, 0.5),
+            anchored_position=(RIGHT_ALIGN_X, y_val - 1)
+        ))
+
+        return slide
+
+    def make_uptown_slide(self, departure):
+        return self.make_slide(departure, self.UP_ARROW, TOP_HALF_Y)
+
+    def make_downtown_slide(self, departure):
+        return self.make_slide(departure, self.DOWN_ARROW, BOTTOM_HALF_Y)
+
+    def make_slide(self, departure, arrow, y_val):
+        slide = []
+
+        slide.append(adafruit_display_text.label.Label(
+            self.SYMBOL_FONT,
+            color=self.TEXT_COLOR,
+            text=arrow,
+            x=ARROW_X,
+            y=y_val
+        ))
+        # Train Symbol
+        slide.append(adafruit_display_text.label.Label(
+            self.SYMBOL_FONT,
+            color=self.TRAIN_COLOR_ORANGE_FM,
+            text=departure['route_id'],
+            x=TRAIN_SYMBOL_X,
+            y=y_val
+        ))
+
+        if departure['departs_in'] == 0:
+            slide.append(adafruit_display_text.label.Label(
+                terminalio.FONT,
+                color=0xaa0000,
+                text="Now",
+                anchor_point=(1.0, 0.5),
+                anchored_position=(RIGHT_ALIGN_X, y_val - 2)
+            ))
+        else:
+            slide.append(adafruit_display_text.label.Label(
+                terminalio.FONT,
+                color=self.TEXT_COLOR,
+                text="{} Min".format(departure['departs_in']),
+                anchor_point=(1.0, 0.5),
+                anchored_position=(RIGHT_ALIGN_X, y_val - 2)
+            ))
+
+        return slide
+
+    # Re-create arrays with the new departure time information
+    def update_board(self, departure_times):
+        # Reset
+        self.uptown_slides = []
+        self.downtown_slides = []
+
+        # Only cycle the first 3 trains
+        for departure in departure_times['uptown'][:3]:
+            self.uptown_slides.append(self.make_uptown_slide(departure))
+        for departure in departure_times['downtown'][:3]:
+            self.downtown_slides.append(self.make_downtown_slide(departure))
+
+        if len(self.uptown_slides) == 0:
+            self.uptown_slides.append(self.make_empty_slide('\uE12b', TOP_HALF_Y))
+        if len(self.downtown_slides) == 0:
+            self.downtown_slides.append(self.make_empty_slide('\uE12c', BOTTOM_HALF_Y))
+
+    # Since slides are stored in array with the first slide being the one displayed, slide advancement is just an array rotation
+    def advance_slides(self):
+        # Clear board by deleting every item in the group
+        # Incredibly hacky but CircuitPy throws a fit (ValueError: Layer already in a group) if this doesn't happen
+        for _ in range(len(self.time_board_group)):
+            del self.time_board_group[0]
+
+        self.uptown_slides = self.uptown_slides[1:] + self.uptown_slides[: 1]
+        self.downtown_slides = self.downtown_slides[1:] + self.downtown_slides[: 1]
+
+        for slide in self.uptown_slides[0]:
+            self.time_board_group.append(slide)
+        for slide in self.downtown_slides[0]:
+            self.time_board_group.append(slide)
+
+    def get_board(self):
+        return self.time_board_group
 
 
-# Bottom Half
-bottom_half = displayio.Group()
-# Down Arrow
-bottom_half.append(adafruit_display_text.label.Label(
-    SYMBOL_FONT,
-    color=TEXT_COLOR,
-    text="\uE12c",
-    x=ARROW_X,
-    y=BOTTOM_HALF_Y
-))
-# Train Symbol
-bottom_half.append(adafruit_display_text.label.Label(
-    SYMBOL_FONT,
-    color=TRAIN_COLOR_ORANGE_FM,
-    text="M",
-    x=TRAIN_SYMBOL_X,
-    y=BOTTOM_HALF_Y
-))
-bottom_half.append(adafruit_display_text.label.Label(
-    terminalio.FONT,
-    color=TEXT_COLOR,
-    text="6 Min",
-    anchor_point=(1.0, 0.5),
-    anchored_position=(RIGHT_ALIGN_X, BOTTOM_HALF_Y - 2)
-))
-
-# Display the groups constructed above
-g = displayio.Group()
-g.append(top_half)
-g.append(bottom_half)
-display.show(g)
-
-
+time_board = TimeBoard(get_departure_times())
+last_refreshed = adafruit_datetime.datetime.now()
 while True:
+    # refresh the API every minute (or so)
+    if adafruit_datetime.datetime.now() - last_refreshed > adafruit_datetime.timedelta(0, API_REFRESH_DELAY, 0):
+        last_refreshed = adafruit_datetime.datetime.now()
+        time_board.update_board(get_departure_times())
+
+    time_board.advance_slides()
+    display.show(time_board.get_board())
+
     display.refresh(minimum_frames_per_second=0)
-    time.sleep(.03)
+    time.sleep(10)
